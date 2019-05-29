be5f8a096fe59439f7e444298dab4cf9820f8a03
shintakezou <shintakezou@gmail.com>
2019-05-29 21:23:06 +0200

    ...

--- src/finalization/my_class.adb
+++ src/finalization/my_class.adb
@@ -2,9 +2,9 @@ with Ada.Unchecked_Deallocation;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Sequential_IO;
 with Interfaces; use Interfaces;
+
 package body My_Class is
-   
-   
+      
    overriding
    procedure Initialize (O : in out Object) is
    begin
@@ -15,12 +15,11 @@ package body My_Class is
    overriding
    procedure Finalize   (O : in out Object) is
       procedure Free is 
-	 new Ada.Unchecked_Deallocation (Buffer, Buffer_Ptr);
+         new Ada.Unchecked_Deallocation (Buffer, Buffer_Ptr);
    begin
       Put_Line ("Freeing");
       Free (O.A);
    end Finalize;
-   
    
    
    procedure Write (O : in Object) is
@@ -30,8 +29,8 @@ package body My_Class is
       
    begin
       for E of O.A.all loop
-      	 --Put_Line (Integer'Image (E));
-	 E := 16#55AABBCC#;
+         --Put_Line (Integer'Image (E));
+         E := 16#55AABBCC#;
       end loop;
       O.A (1) := Unsigned_32'Last;
       O.A (2) := Unsigned_32'First;
@@ -43,7 +42,6 @@ package body My_Class is
       --  I doubt this is the way to produce a portable binary file
       --  which can be read by a similar program on every other machine
       Buf_IO.Close (H);
-   end Write;
-   
+   end Write;   
    
 end My_Class;
